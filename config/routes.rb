47e92743e1cf1f51c0dcb8ba9d3c6890ae7fe1c9
Raymund Werling <raymund.werling@googlemail.com>
2022-11-15 16:54:21 +0100

    task list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ('/')
+  # root 'articles#index'
+
+  # create
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: 'tasks#create'
+
+  # read
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks', to: 'tasks#index', as: 'index'
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
